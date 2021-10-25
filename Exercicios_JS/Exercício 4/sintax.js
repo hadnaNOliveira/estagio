3cd8d815112a682aa96f3c3995cf5b3b573bbd68
hadnaNOlii <nanda.oliveira.pereira@gmail.com>
2021-10-25 11:39:17 -0300

    correçao exercicio 4

--- Exercicios_JS/Exercício 4/sintax.js
+++ Exercicios_JS/Exercício 4/sintax.js
@@ -3,25 +3,29 @@ var opt = prompt("Informe a operação desejada")
 var n2 = Number(prompt("Insira um valor:"))
 
 
-var value1 = n1
-var value2 = n2 
+if (typeof n1 === Number && n1.toString() !== NaN
+    && typeof n2 === Number && n2.toString() !== NaN) {
 
-if(opt === "+") {
-    var resultado = n1 + n2
-    alert(resultado)
-}
-if (opt === "-") {
-    var resultado = n1 - n2
-    alert(resultado)
-}
-if (opt === "*") {
-    var resultado = n1 * n2
-    alert(resultado)
-}
-if (opt === "/") {
-    var resultado = n1 / n2
-    alert(resultado)
+        if(opt === "+") {
+            var resultado = n1 + n2
+            alert(resultado)
+        }
+        if (opt === "-") {
+            var resultado = n1 - n2
+            alert(resultado)
+        }
+        if (opt === "*") {
+            var resultado = n1 * n2
+            alert(resultado)
+        }
+        if (opt === "/" && n1 !== "0" && n2 !== "0") {
+            var resultado = n1 / n2
+            alert(resultado)
+        }
 }
 else if (opt !== "+" && opt !== "-" && opt !== "/" && opt !== "*") {
     prompt("Por favor, insira um operador válido")
+}
+else {
+    alert("Utilize apenas números, para realizar a operação desejada.")
 }
